3561e65049e06ca8e987e628b44ce74785b8de1d
Stéphane Klein <contact@stephane-klein.info>
2023-08-25 19:48:22 +0200

    Implement alter column if exists

--- migrations/current.sql
+++ migrations/current.sql
@@ -1,3 +1,18 @@
 -- Enter migration here
 ALTER TABLE main.contacts ADD COLUMN IF NOT EXISTS firstname VARCHAR DEFAULT '';
-ALTER TABLE main.contacts RENAME name TO lastname;
+
+-- boilerplate found here: https://www.codingvila.com/2021/05/rename-column-only-if-exists-in-postgresql.html
+DO $$
+BEGIN
+    IF EXISTS(
+        SELECT *
+            FROM information_schema.columns
+            WHERE (
+                (table_schema='main') AND
+                (table_name='contacts') AND
+                (column_name='name')
+            )
+  ) THEN
+      ALTER TABLE main.contacts RENAME name TO lastname;
+  END IF;
+END $$;
